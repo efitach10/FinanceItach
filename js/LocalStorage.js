4d4ada963543f25da763669a87262c951f610d09
efitach10 <128313628+efitach10@users.noreply.github.com>
2026-02-27 16:54:35 +0200

    Add files via upload

---
+++ js/LocalStorage.js
@@ -0,0 +1,12 @@
+const StorageManager = {
+
+    get(key) {
+        const data = localStorage.getItem(key);
+        return data ? JSON.parse(data) : [];
+    },
+
+    set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+};
